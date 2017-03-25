5bf1d9add289e84c7f96f52f769f3109d4f3a661
cfadel1 <cedricfadel@hotmail.com>
2017-03-25 13:24:49 -0400

    Porting over PersonalWebsite to Github

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//change this shit to window and not document 
+$(document).ready(function () {
+    $('.loader').fadeOut(5000, "swing");
+
+    $(document).ready(function () {
+        $('a[href^="#"]').on('click', function (e) {
+            e.preventDefault();
+            var target = this.hash;
+            $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 900, 'swing', function () {
+                window.location.hash = target;
+            });
+        });
+    });
+});
